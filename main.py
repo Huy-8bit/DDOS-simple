a26f20dcdd2ef06d46e042fd2e356fb131295fa5
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2024-02-27 04:03:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+import requests
+from concurrent.futures import ThreadPoolExecutor
 
-app = FastAPI()
-a = 0
-@app.get("/")
-async def read_root():
-    global a  # Khai báo rằng `a` là một biến global
-    a += 1
-    return {"Hello": f"World {a}"}
+url = 'your_url'
+
+def send_request(url):
+    response = requests.get(url)
+    print('Status Code:', response.status_code)
+    print('Response Body:', response.text)
+
+
+threads = 10000000
+
+with ThreadPoolExecutor(max_workers=threads) as executor:
+    for _ in range(threads):
+        executor.submit(send_request, url)
